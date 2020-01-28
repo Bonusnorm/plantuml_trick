368d8dbdabe6c389d437f2bbff5f2f41fcbddc6b
iilei • Jochen Preusche <iilei@users.noreply.github.com>
2020-01-28 20:09:04 +0100

    wip

--- setup.py
+++ setup.py
@@ -101,7 +101,7 @@ setup(
     # keywords='sample setuptools development',  # Optional
     # When your source code is in a subdirectory under the project root, e.g.
     # `src/`, it is necessary to specify the `package_dir` argument.
-    # package_dir={"": "plantuml_trick"},  # Optional
+    package_dir={"": "plantuml_trick"},  # Optional
     # You can just specify package directories manually here if your project is
     # simple. Or you can use find_packages().
     #
@@ -111,7 +111,7 @@ setup(
     #
     #   py_modules=["my_module"],
     #
-    packages=find_packages("."),  # Required
+    packages=find_packages(where="plantuml_trick"),  # Required
     # Specify which Python versions you support. In contrast to the
     # 'Programming Language' classifiers above, 'pip install' will check this
     # and refuse to install the project if the version does not match. If you
